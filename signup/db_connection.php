ac8574ca3ea8561d120cb3e6a828b1dfb75884a4
MOHAMMED THOUFEEK A <www.thoufeekabc@gmail.com>
2023-08-20 20:28:48 +0530

    verify

--- signup/db_connection.php
+++ signup/db_connection.php
@@ -1,9 +1,9 @@
 
 <?php
-$host = 'localhost';
+$host = 'dpg-cjh2hg337aks7385gukg-a';
 $db = 'login_signup'; // Name of the database you created
 $user = 'root';     // Default MySQL user in XAMPP
-$password = '';     // Default MySQL password in XAMPP
+$password = 'SKHaGBQlhlGiGnltO6vp3509PkcoA8Hb';     // Default MySQL password in XAMPP
 
 $conn = mysqli_connect($host, $user, $password, $db);
 
